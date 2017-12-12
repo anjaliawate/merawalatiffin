9c27bcb21ced0a9768b3d0cc85feecac16122ac2
Anjali Awate <awateanjali123@gmail.com>
2017-12-12 12:06:33 +0530

    first_commit

---
+++ www/js/contactController.js
@@ -0,0 +1,40 @@
+angular.module('merawalatiffin.controllers', [])
+
+
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  .controller('ContactCtrl', function($scope, $stateParams) {
+  $scope.orderData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/contact.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  // $scope.closeLogin = function() {
+  //   $scope.modal.hide();
+
+  // };
+
+  // Perform the order action when the user submits the order form
+  $scope.contact = function() {
+    console.log('Doing order', $scope.orderData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    // $timeout(function() {
+    //   $scope.closeLogin();
+    // }, 1000);
+    };
+
+ });
